de6439f398577c2dc3b5d0a549f8d6b9c83777b0
gouranga <gm49302@gmail.com>
2023-11-26 22:22:49 +0530

    basic crud with session and cookie

---
+++ lolopopo/routes/users.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/practice");
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  age: Number,
+});
+module.exports = mongoose.model("user", userSchema);
